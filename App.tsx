6586536a55295520baef84d9e93c0fc586a7150b
Kicknee <daniszewski21@gmail.com>
2024-08-19 23:51:59 +0200

    clear App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {Image, SafeAreaView, StyleSheet, Text} from 'react-native';
 
 function App(): React.JSX.Element {
