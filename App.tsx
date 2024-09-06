1d1a8326525d0679aa6c32c8ea27e89830cb58a2
Kicknee <daniszewski21@gmail.com>
2024-09-06 16:22:39 +0200

    style logo image

--- App.tsx
+++ App.tsx
@@ -1,18 +1,46 @@
 import React from 'react';
-import {Image, SafeAreaView, StyleSheet, Text} from 'react-native';
+import {
+  Alert,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView style={{backgroundColor: 'black'}}>
-      <Image source={require('./assets/bonjour-logo.png')} />
-      <Text>Hello!</Text>
+    <SafeAreaView style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require('./assets/bonjour-logo.png')}
+      />
+      <TouchableOpacity
+        onPress={() => {
+          Alert.alert('swe');
+        }}>
+        <Text style={styles.scanButton}>SCAN</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
-  title: {},
+  container: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'black',
+    alignItems: 'center',
+  },
+  logo: {
+    height: '40%',
+    aspectRatio: '1 / 1',
+    marginTop: 30,
+  },
+  scanButton: {
+    alignSelf: 'flex-end',
+    color: 'red',
+  },
 });
 
 export default App;
