726cc682a8e4bbef3026dd29f22251384b7cbeb2
Kicknee <daniszewski21@gmail.com>
2025-01-15 20:09:54 +0100

    add logo to the home page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,28 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
+const logo = require("../assets/images/bonjour-logo.png");
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Image alt="logo" source={logo} style={{ width: 300, height: 300 }} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "black",
+    alignItems: "center",
+  },
+  logo: {
+    height: "40%",
+    aspectRatio: "1 / 1",
+    marginTop: 30,
+  },
+  scanButton: {
+    color: "red",
+    marginTop: 70,
+  },
+});
