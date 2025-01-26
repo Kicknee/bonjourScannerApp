be178c10476566ff5d85ba060fa89c1a84ad90cc
Kicknee <daniszewski21@gmail.com>
2025-01-26 20:21:32 +0100

    display scan button based on permission status

--- app/index.tsx
+++ app/index.tsx
@@ -4,57 +4,53 @@ import {
   Image,
   StyleSheet,
   Button,
-  Alert,
   TouchableOpacity,
 } from "react-native";
 import { CameraView, CameraType, useCameraPermissions } from "expo-camera";
-import Camera from "./camera/camera";
+import ScannerQR from "./camera/ScannerQR";
+import { useState } from "react";
 
 const logo = require("../assets/images/bonjour-logo.png");
 
 export default function Index() {
   const [permission, requestPermission] = useCameraPermissions();
-
-  if (!permission) {
-    // Camera permissions are still loading.
-    return <View />;
-  }
-
-  if (!permission.granted) {
-    // Camera permissions are not granted yet.
-    return (
-      <View style={styles.container}>
-        <Text>We need your permission to show the camera</Text>
-        <Button onPress={requestPermission} title="grant permission" />
-      </View>
-    );
-  }
+  const [enableScannerQR, setEnableScanner] = useState(false);
 
   return (
     <View style={styles.container}>
       <Image alt="logo" source={logo} style={styles.logo} />
-      <TouchableOpacity
-        style={styles.scanButtonContainer}
-        onPress={() => {
-          Alert.alert("SCAN");
-        }}
-      >
-        <Text style={styles.scanButtonText}>SCAN</Text>
-      </TouchableOpacity>
-      <Camera />
+      {permission && !permission.granted ? (
+        <TouchableOpacity
+          style={styles.scanButtonContainer}
+          onPress={requestPermission}
+        >
+          <Text style={styles.buttonText}>GRANT PERMISSION</Text>
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity
+          style={styles.scanButtonContainer}
+          onPress={() => {
+            setEnableScanner(true);
+          }}
+        >
+          <Text style={styles.buttonText}>SCAN</Text>
+        </TouchableOpacity>
+      )}
+      {!!enableScannerQR && <ScannerQR setEnableScanner={setEnableScanner} />}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     width: "100%",
     height: "100%",
     backgroundColor: "black",
     alignItems: "center",
   },
   logo: {
-    minHeight: "40%",
+    height: "40%",
     aspectRatio: "1 / 1",
     marginTop: 30,
   },
@@ -66,24 +62,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#D9D9D9B3", // 70% opacity
   },
-  scanButtonText: {
+  buttonText: {
     fontSize: 24,
     color: "#FFF",
-  },
-  buttonContainer: {
-    flex: 1,
-    flexDirection: "row",
-    backgroundColor: "transparent",
-    margin: 64,
-  },
-  button: {
-    flex: 1,
-    alignSelf: "flex-end",
-    alignItems: "center",
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "white",
   },
 });
