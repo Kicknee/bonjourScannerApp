d284cfbbf2580a800069f3e84602d84a9576bab0
Kicknee <daniszewski21@gmail.com>
2025-02-05 23:56:37 +0100

    replace overlay custom style with react native svg

--- app/camera/Overlay.tsx
+++ app/camera/Overlay.tsx
@@ -1,40 +1,47 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
+import Svg, { Defs, ClipPath, Polygon, Rect } from "react-native-svg";
 
 const Overlay = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.mask} />
-      <View style={styles.scannerFrameContainer}>
-        <View />
-      </View>
-      <View style={styles.mask} />
+      <Svg style={StyleSheet.absoluteFill} viewBox="0 0 100 100">
+        <Defs>
+          <ClipPath id="clipShape">
+            <Polygon points="0,0 0,100 25,100 25,25 75,25 75,75 25,75 25,100 100,100 100,0" />
+          </ClipPath>
+        </Defs>
+        {/* Czarna maska */}
+        <Rect
+          width="100%"
+          height="100%"
+          fill="rgba(0, 0, 0, 0.7)"
+          clipPath="url(#clipShape)"
+        />
+      </Svg>
+
+      {/* Kwadratowa ramka skanera */}
+      <View style={styles.scannerFrame} />
     </View>
   );
 };
-
+//clip-path: polygon(0% 0%, 0% 100%, 25% 100%, 25% 25%, 75% 25%, 75% 75%, 25% 75%, 25% 100%, 100% 100%, 100% 0%);
 export default Overlay;
+
+const SCANNER_SIZE = 250; // Rozmiar kwadratu do skanowania
 
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    flexDirection: "column",
-  },
-  mask: {
-    flex: 1,
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-  },
-  scannerFrameContainer: {
-    height: 270,
     justifyContent: "center",
     alignItems: "center",
-    alignSelf: "center",
   },
-  // scannerFrame: {
-  //   width: 270,
-  //   height: 270,
-  //   borderWidth: 2,
-  //   borderColor: "#00FF00",
-  //   borderRadius: 8,
-  // },
+  scannerFrame: {
+    width: SCANNER_SIZE,
+    height: SCANNER_SIZE,
+    borderWidth: 2,
+    borderColor: "#00FF00", // Zielona ramka
+    borderRadius: 8,
+    position: "absolute",
+  },
 });
