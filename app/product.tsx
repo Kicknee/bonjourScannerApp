39b0032b4ea661085c949c979299a2576beaeb36
Kicknee <daniszewski21@gmail.com>
2025-02-17 12:10:34 +0100

    display passed scanned data on product page

--- app/product.tsx
+++ app/product.tsx
@@ -1,9 +1,33 @@
+import { useLocalSearchParams } from "expo-router";
 import { Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 
 export default function ProductScreen() {
+  const params = useLocalSearchParams();
   return (
-    <View>
-      <Text>Product Details</Text>
+    <View style={styles.container}>
+      <Text style={styles.header}>Product Details</Text>
+      <Text>Style: {params.STYLE}</Text>
+      {/* <Text>Type: {params.TYPE}</Text>
+      <Text>Place: {params.PLACE}</Text>
+      <Text>Left: {params.LEFT}</Text>
+      <Text>Color: {params.COLOR}</Text>
+      <Text>Brand: {params.BRAND}</Text>
+      <Text>Shipping Company: {params.SHIPPING_COMPANY}</Text> */}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+});
