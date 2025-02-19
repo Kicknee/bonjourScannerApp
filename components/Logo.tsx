fa9d03a2b5fcb5fea14df37b32ea25a8b2d1a5fd
Kicknee <daniszewski21@gmail.com>
2025-02-19 14:35:02 +0100

    create logo image as a separate componnent

---
+++ components/Logo.tsx
@@ -0,0 +1,24 @@
+import { Image, StyleSheet } from "react-native";
+
+const LogoSource = require("../assets/images/bonjour-logo.png");
+
+type Props = {
+  height: number;
+};
+
+export default function Logo({ height }: Props) {
+  return (
+    <Image
+      alt="logo"
+      source={LogoSource}
+      style={[styles.logo, { height: `${height}%` }]}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  logo: {
+    aspectRatio: "1 / 1",
+    marginTop: "30%",
+  },
+});
