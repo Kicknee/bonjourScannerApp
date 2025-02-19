0601cb216c3f6f611fb2cafe2f67b0c8cdbc5882
Kicknee <daniszewski21@gmail.com>
2025-02-19 20:45:11 +0100

    style button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,8 +6,23 @@ type Props = {
 };
 export default function Button({ label, onPress }: Props) {
   return (
-    <TouchableOpacity onPress={onPress}>
-      <Text>{label}</Text>
+    <TouchableOpacity style={styles.scanButtonContainer} onPress={onPress}>
+      <Text style={styles.buttonText}>{label}</Text>
     </TouchableOpacity>
   );
 }
+
+const styles = StyleSheet.create({
+  scanButtonContainer: {
+    marginTop: 70,
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    borderRadius: 3,
+    alignItems: "center",
+    backgroundColor: "#D9D9D9B3", // 70% opacity
+  },
+  buttonText: {
+    fontSize: 21,
+    color: "#FFF",
+  },
+});
