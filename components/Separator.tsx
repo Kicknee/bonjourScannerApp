3d925530a615e00daa113dd5e19a595075dda315
Kicknee <daniszewski21@gmail.com>
2025-02-19 21:10:03 +0100

    create and style Separator component

---
+++ components/Separator.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+type Props = {
+  marginVertical: number;
+};
+
+export default function Separator({ marginVertical }: Props) {
+  return <View style={{ marginVertical: marginVertical }} />;
+}
